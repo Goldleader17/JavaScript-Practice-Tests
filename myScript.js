ca2e781cf6c9e62c6de6ec46c26cc7557199626c
Gabriel Ezerioha <jc6364972@gmail.com>
2023-06-06 12:57:22 +0100

    first commit

---
+++ myScript.js
@@ -0,0 +1,9 @@
+function myFunction() {
+
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+function myFunction() {
+
+    document.getElementById("demo1").innerHTML = "Hello Dolly!";
+    document.getElementById("demo2").innerHTML = "How are you?";
+}
---
+++ script.js
@@ -0,0 +1,13 @@
+function Hello()
+{
+ alert("Hello, World");
+}
+
+// How to create variables:
+var x;
+var y;
+
+// How to use variables:
+x = 5;
+y = 6;
+let z = x + y;
